ee375667fffb47e23288e82152f5327d14dc1fae
KaHei Ng <kaheicanaan@gmail.com>
2020-12-27 00:38:54 +0800

    Create DB session dependency.

--- server/database_schemas/base.py
+++ server/database_schemas/base.py
@@ -15,4 +15,21 @@ SQLALCHEMY_DATABASE_URL = (f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{P
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+
+class DBSession(object):
+    def __init__(self):
+        self.db = SessionLocal()
+
+    def __enter__(self):
+        return self.db
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db.close()
+
+
+def get_db_session():
+    with DBSession() as db:
+        yield db
+
+
 Base = declarative_base()
