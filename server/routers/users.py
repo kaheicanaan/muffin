7c98c75abd661e27914d8a85427022dd6d047f8c
KaHei Ng <kaheicanaan@gmail.com>
2020-12-27 12:24:13 +0800

    Fix duplicated function name.

--- server/routers/users.py
+++ server/routers/users.py
@@ -34,7 +34,7 @@ router = APIRouter()
 
 
 @router.post("/")
-def create_user(user: UserCreate, db: Session = Depends(get_db_session)):
+def create_new_user(user: UserCreate, db: Session = Depends(get_db_session)):
     db_user = get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
