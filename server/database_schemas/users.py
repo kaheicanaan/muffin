c04c18200a732c34cfa91633f5c89dc22450c86e
KaHei Ng <kaheicanaan@gmail.com>
2020-12-28 00:15:56 +0800

    Update users table schema.

--- server/database_schemas/users.py
+++ server/database_schemas/users.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy import Boolean, Column, String
 from sqlalchemy.dialects.postgresql import UUID
 
@@ -7,7 +9,7 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID, primary_key=True, index=True)
+    id = Column(UUID, primary_key=True, index=True, default=uuid.uuid4)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
