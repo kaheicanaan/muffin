8e8c980cad6957756ff3536688e24a5f89284315
KaHei Ng <kaheicanaan@gmail.com>
2020-12-27 12:02:16 +0800

    Add create_user and read_user_by_email endpoints.

--- server/routers/users.py
+++ server/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import Depends, APIRouter, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from data_models.users import UserCreate
@@ -33,9 +33,17 @@ def count_users(db: Session):
 router = APIRouter()
 
 
-@router.get("/{user_id}")
-async def read_user(user_id: str, db: Session = Depends(get_db_session)):
-    db_user = get_user_by_id(db, user_id)
+@router.post("/")
+def create_user(user: UserCreate, db: Session = Depends(get_db_session)):
+    db_user = get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
+    return create_user(db=db, user=user)
+
+
+@router.get("/{user_email}")
+def read_user(user_email: str, db: Session = Depends(get_db_session)):
+    db_user = get_user_by_email(db, email=user_email)
     if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return db_user
