3a3d9d0438e03b724be2cff342a8cd000af5c878
Cherry Ng <cherrry@users.noreply.github.com>
2020-12-27 20:34:27 +0800

    Initial setup of git hooks. (#9)
    * Create local git hooks to prevent commiting to master branch."
    
    * Add git hook to run pylint before commit.
    
    * Fix git hooks check.

--- dev.sh
+++ dev.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+ROOT="$(git rev-parse --show-toplevel)"
+cd "${ROOT}"
+
+# Ensure git hooks.
+if [[ -d "${ROOT}/.git/hooks" && ! -L "${ROOT}/.git/hooks" ]]; then
+  echo "Setup git hooks ..."
+  mv "${ROOT}/.git/hooks" "${ROOT}/.git/hooks_default"
+  ln -s "${ROOT}/git_hooks" "${ROOT}/.git/hooks"
+fi
+
+# Start local dev servers.
 docker-compose --env-file ./.env.dev up \
   --force-recreate \
   --detach
