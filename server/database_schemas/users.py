d1eb1768cd96d5222eaa24b84e1d168d6b52c7be
Cherry Ng <cherrry@users.noreply.github.com>
2020-12-28 16:30:40 +0800

    Integrate black to auto format. (#13)

--- server/database_schemas/users.py
+++ server/database_schemas/users.py
@@ -9,10 +9,7 @@ from .base import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(UUID(as_uuid=True),
-                primary_key=True,
-                index=True,
-                default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
