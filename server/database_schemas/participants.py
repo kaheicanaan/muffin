2af2ab5ca6f81db20e28b857dc9c31e63b234483
KaHei Ng <kaheicanaan@gmail.com>
2021-01-06 00:06:49 +0800

    Add participant table for user-room relationship.

---
+++ server/database_schemas/participants.py
@@ -0,0 +1,25 @@
+import enum
+
+from sqlalchemy import Column, Enum, ForeignKey, Integer
+from sqlalchemy.orm import relationship
+
+from .base import Base
+
+
+class Role(enum.Enum):
+    member = "MEMBER"
+    owner = "OWNER"
+    admin = "ADMIN"
+    announcer = "ANNOUNCER"
+
+
+class ParticipantEntry(Base):
+    __tablename__ = "participants"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+    room_id = Column(Integer, ForeignKey("rooms.id"), index=True)
+    role = Column(Enum(Role), nullable=False)
+
+    user = relationship("UserEntry", back_populates="rooms")
+    room = relationship("RoomEntry", back_populates="users")
