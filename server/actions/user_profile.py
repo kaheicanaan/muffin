31c03cf730df5b51d8b612e0404f250576df4638
KaHei Ng <kaheicanaan@gmail.com>
2021-01-07 14:09:04 +0800

    Raise exception in user profile.

--- server/actions/user_profile.py
+++ server/actions/user_profile.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
@@ -18,10 +16,14 @@ class UserProfile(object):
     def __init__(self, db: Session = Depends(db_session)):
         self.db = db
 
-    # pylint: disable=unsubscriptable-object
-    def find_by_id(self, user_id: int) -> Optional[UserEntry]:
-        return self.db.query(UserEntry).filter(UserEntry.id == user_id).first()
+    def find_by_id(self, user_id: int) -> UserEntry:
+        user_entry = self.db.query(UserEntry).filter(UserEntry.id == user_id).first()
+        if user_entry is None:
+            raise UserNotFoundException(user_id=user_id)
+        return user_entry
 
-    # pylint: disable=unsubscriptable-object
-    def find_by_email(self, email: str) -> Optional[UserEntry]:
-        return self.db.query(UserEntry).filter(UserEntry.email == email).first()
+    def find_by_email(self, email: str) -> UserEntry:
+        user_entry = self.db.query(UserEntry).filter(UserEntry.email == email).first()
+        if user_entry is None:
+            raise UserNotFoundException(user_email=email)
+        return user_entry
--- server/actions/user_registration.py
+++ server/actions/user_registration.py
@@ -2,7 +2,7 @@ from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from actions.user_authentication import hash_password
-from actions.user_profile import UserProfile
+from actions.user_profile import UserProfile, UserNotFoundException
 from data_models.users import UserCreate
 from database_schemas.db_session import db_session
 from database_schemas.users import UserEntry
@@ -20,8 +20,14 @@ class UserRegistration(object):
         self.user_profile = user_profile
 
     def create_user(self, user: UserCreate) -> UserEntry:
-        if self.user_profile.find_by_email(user.email):
+        # ensure user does not exist
+        try:
+            self.user_profile.find_by_email(user.email)
+        except UserNotFoundException:
+            pass
+        else:
             raise UserAlreadyExistsException()
+
         hashed_password = hash_password(user.password)
         user_entry = UserEntry(email=user.email, hashed_password=hashed_password)
         self.db.add(user_entry)
