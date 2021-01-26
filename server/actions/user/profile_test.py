3772ed6e8bed431b59ed29382577062bc19309f9
KaHei Ng <kaheicanaan@gmail.com>
2021-01-26 23:09:48 +0800

    Add updating existing email test case.

--- server/actions/user/profile_test.py
+++ server/actions/user/profile_test.py
@@ -1,6 +1,10 @@
 import pytest
 
-from actions.user.profile import UserProfile, UserNotFoundException
+from actions.user.profile import (
+    UserProfile,
+    UserNotFoundException,
+    EmailAlreadyUsedException,
+)
 from database_schemas.users import UserEntry
 
 
@@ -32,6 +36,15 @@ class TestUpdateProfile(object):
         _ = user_profile.update_email(user_entry_squirtle.id, new_email)
         assert user_entry_squirtle.email != old_email
         assert user_entry_squirtle.email == new_email
+
+    def test_update_existing_email(
+        self,
+        user_profile: UserProfile,
+        user_entry_squirtle: UserEntry,
+        user_entry_zenigame: UserEntry,
+    ):
+        with pytest.raises(EmailAlreadyUsedException):
+            user_profile.update_email(user_entry_squirtle.id, user_entry_zenigame.email)
 
     def test_update_password(
         self,
