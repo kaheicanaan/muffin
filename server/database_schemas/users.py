d8e093bc1aa011d1499e11a0d97af496afabaf20
KaHei Ng <kaheicanaan@gmail.com>
2021-02-01 22:00:39 +0800

    Add username in user database schema and data model.

--- server/database_schemas/users.py
+++ server/database_schemas/users.py
@@ -8,6 +8,7 @@ class UserEntry(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(LargeBinary)
     is_active = Column(Boolean, default=True)
